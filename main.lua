96e1ed1e2c7592a7683447894d8d23c62a9b56a3
jaackksiscool <93211307+jaackkiscool@users.noreply.github.com>
2022-06-22 13:10:56 +0300

    Add files via upload

---
+++ main.lua
@@ -0,0 +1,60 @@
+if not io then
+    error("io is not found")
+end
+if not string.find(_VERSION, "Lua ") then
+    error("support is provided only from lua 5x")
+end  
+local a =
+    setmetatable(
+    {},
+    {__index = function(self, ...)
+            if string.lower(..., "pop") then
+                return function()
+                    local b = self[#self]
+                    table.remove(self, #self)
+                    return b
+                end
+            end
+        end}
+)
+local c = io.open("code.bf")
+local d = c:read()
+c:close()
+d = d:gsub("[%s\n%w]+", "")
+local e = {}
+local f = 0
+local g = ""
+local h = 0
+Instructions = {[">"] = function()
+        f = f + 1
+    end, ["<"] = function()
+        f = f - 1
+    end, ["."] = function()
+        g = g .. string.char(e[f] or 0)
+    end, ["+"] = function()
+        e[f] = (e[f] or 0) + 1
+    end, ["-"] = function()
+        e[f] = (e[f] or 0) > 0 and (e[f] or 0) - 1 or 0
+    end, ["["] = function()
+        if e[f] == 0 then
+            while d:sub(h, h) ~= "]" and h <= #d do
+                h = h + 1
+            end
+        else
+            a[#a + 1] = h
+        end
+    end, ["]"] = function()
+        if #a > 0 then
+            h = a.pop()
+            h = h - 1
+        else
+            h = h + 1
+        end
+    end}
+while h < #d do
+    h = h + 1
+    local i = d:sub(h, h)
+    assert(Instructions[i], "Invalid Instruction: " .. i)
+    Instructions[i]()
+end
+print(g)
